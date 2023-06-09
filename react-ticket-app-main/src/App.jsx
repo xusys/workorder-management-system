a3111994d984208afcd44fdf476628f0c4a1f0bd
DDDFXYqiming <dddfxy@qq.com>
2023-06-10 00:47:53 +0800

    测试推送

--- react-ticket-app-main/src/App.jsx
+++ react-ticket-app-main/src/App.jsx
@@ -10,8 +10,10 @@ export default function App() {
 
   return (
     <div className="App" style={{minHeight: '100vh'}}>
+    <h1>推送</h1>
     {/* 注册路由 */}
     {element}
     </div>
+    // haohaohao
   );
 }
