410ce668683b3123ff881e76c2f2f44c46bac2de
CSPureWhite <1461459810@qq.com>
2023-06-17 18:14:07 +0800

    commit by ysx

--- src/main/java/com/example/demo/config/WebMvcConfig.java
+++ src/main/java/com/example/demo/config/WebMvcConfig.java
@@ -47,7 +47,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     public void addInterceptors(InterceptorRegistry registry){
         List<String> excludePath = new ArrayList<>();
         // 排除登录、注册页面的拦截
-        excludePath.add("/user/*");
+        excludePath.add("/user/**");
         registry.addInterceptor(new JwtInterceptor())
                 .addPathPatterns("/**")
                 .excludePathPatterns(excludePath);
@@ -59,7 +59,8 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
      */
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")// 项目中的所有接口都支持跨域
-                .allowedOrigins("*") //允许哪些域能访问我们的跨域资源
+                .allowCredentials(true)
+                .allowedOrigins("http://localhost:8080") //允许哪些域能访问我们的跨域资源
                 .allowedMethods("*")//允许的访问方法"POST", "GET", "PUT", "OPTIONS", "DELETE"等
                 .allowedHeaders("*");//允许所有的请求header访问，可以自定义设置任意请求头信息
     }
--- src/main/java/com/example/demo/interceptor/JwtInterceptor.java
+++ src/main/java/com/example/demo/interceptor/JwtInterceptor.java
@@ -1,5 +1,6 @@
 package com.example.demo.interceptor;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.example.demo.utils.JwtUtil;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -13,9 +14,16 @@ public class JwtInterceptor implements HandlerInterceptor {
         String token=request.getHeader("token");
         try{
             JwtUtil.verifyToken(token);
+            System.out.println(token);
             return true;
-        }catch (Exception Exception){
+        }catch (TokenExpiredException Exception){
             response.sendError(401);
+            System.out.println("no");
+            return false;
+        }
+        catch (Exception e){
+            response.sendError(401);
+            System.out.println("null");
             return false;
         }
     }
